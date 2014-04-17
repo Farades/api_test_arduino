f36d4be74b6e5f6b6948e6a5adcde5cc1c508211
farades <creg2007@gmail.com>
2014-04-17 19:10:26 +0400

    add serial buffer and parsing query

--- api_test.ino
+++ api_test.ino
@@ -1,6 +1,12 @@
 /*-----------------------------------------------------
 parametr1 - вкл/выкл светодиод
 parametr2 - значение потенциометра в пределах [0; 255]
+Формат строки:
+DID=1;CMD=2;PRM=2;DAT=123
+Для включения светодиода:
+DID=1;CMD=1;PRM=1;DAT=1
+Для выключения светодиода:
+DID=1;CMD=1;PRM=1;DAT=0
 ------------------------------------------------------*/
 
 int led_pin = 13;
@@ -11,6 +17,10 @@ int previous_pot_val = 0;
 int pot_sensitivity = 2;
 //Device ID
 byte DID = 1;
+char DID_in[10];
+char CMD_in[10];
+char PRM_in[10];
+char DAT_in[100];
 
 void setup()
 {
@@ -20,25 +30,30 @@ void setup()
 
 void loop()
 {
+  int i=0;
+  char buffer[100];
   if (Serial.available() > 0)
   {
-    int read_val = Serial.read() - '0';
-    if (read_val == 1)
+    delay(100);
+    while( Serial.available() && i< 99) 
     {
-      digitalWrite(led_pin, HIGH);
-      Serial.print("DID=");
-      Serial.print(DID);
-      Serial.println(";parameter=2;value=1");
-      Serial.println("-----------");
+      buffer[i++] = Serial.read();
     }
-    else if (read_val == 0)
-    {
-      digitalWrite(led_pin, LOW);
-      Serial.print("DID=");
-      Serial.print(DID);
-      Serial.println(";parameter=2;value=0");
-      Serial.println("-----------");
-    }
+    buffer[i++]='\0';
+  }
+  if(i>0)
+  {     
+    Serial.println(String(buffer));
+    //разбераем его на части отделенные запятой
+    sscanf(buffer, "%*c%*c%*c%*c%[^';'];%*c%*c%*c%*c%[^';'];%*c%*c%*c%*c%[^';'];%*c%*c%*c%*c%s",
+      &DID_in, &CMD_in, &PRM_in, &DAT_in);
+   
+    if (((String)DAT_in == "1") && ((String)CMD_in == "1") && ((String)PRM_in == "1")) digitalWrite(led_pin, HIGH);
+    if (((String)DAT_in == "0") && ((String)CMD_in == "1") && ((String)PRM_in == "1")) digitalWrite(led_pin, LOW);
+    Serial.println(DID_in);   
+    Serial.println(CMD_in);
+    Serial.println(PRM_in); 
+    Serial.println(DAT_in);
   }
   int pot_val = map(analogRead(pot_pin), 0, 1023, 0, 255);
   if (abs(pot_val - previous_pot_val) > pot_sensitivity)
@@ -46,9 +61,8 @@ void loop()
     previous_pot_val = pot_val;
     Serial.print("DID=");
     Serial.print(DID);
-    Serial.print(";parameter=1;value=");
+    Serial.print(";CMD=2;PRM=2;DAT=");
     Serial.println(pot_val);
-    Serial.println("-----------");
   }  
 }
 
